a3e1098b9785bed5587d346e4a2503093e4924b7
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2022-12-26 21:21:48 -0300

    add check old password

--- projeto-09/src/controllers/UserController.js
+++ projeto-09/src/controllers/UserController.js
@@ -1,6 +1,6 @@
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite')
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 
 class UserController{
   async create(request, response){  
@@ -22,7 +22,7 @@ class UserController{
   }
 
   async update(request, response){
-    const { name, email } = request.body //Informações que queremos por enquanto
+    const { name, email, password, old_password } = request.body //Informações que queremos por enquanto
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -42,13 +42,28 @@ class UserController{
     user.name = name;
     user.email = email;
 
+    if(password && !old_password){
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
+    }
+
+    if(password && old_password){                          //Resgatamos do nosso banco de dados
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword){
+        throw new AppError("Senha antiga não confere.")
+      }
+    }
+
+    user.password = await hash(password, 8)
+
     await database.run(`
       UPDATE users SET
       name = ?,
       email = ?,
+      password = ?,
       updated_at = ?
       WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, new Date(), id]
     );
 
     return response.json()
