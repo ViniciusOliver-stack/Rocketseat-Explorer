0abb9c1c6bfe6637f08599a496caabee21fb4067
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2022-12-28 20:22:11 -0300

    Added functionality: delete, register, display and list all notes

--- projeto-09/src/controllers/MovieNotesController.js
+++ projeto-09/src/controllers/MovieNotesController.js
@@ -1,11 +1,12 @@
+const { response } = require('express')
 const knex = require('../database/knex')
 
-class MovieNotesController{
-  async create(request, response){
+class MovieNotesController {
+  async create(request, response) {
     const { title, description, rating, movietag } = request.body
     const { user_id } = request.params
 
-    const note_id = await knex("movieNotes").insert({
+    const note_id = await knex('movieNotes').insert({
       title,
       description,
       rating,
@@ -13,16 +14,49 @@ class MovieNotesController{
     })
 
     const movieTagsInsert = movietag.map(name => {
-      return{
+      return {
         name,
         user_id,
         note_id
       }
     })
 
-    await knex("movieTags").insert(movieTagsInsert)
+    await knex('movieTags').insert(movieTagsInsert)
 
     response.json()
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+
+    const noteMovie = await knex('movieNotes').where({ id }).first()
+
+    const tagsMovie = await knex('movieTags')
+      .where({ note_id: id })
+      .orderBy('name')
+
+    return response.json({
+      ...noteMovie,
+      tagsMovie
+    })
+  }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex('movieNotes').where({ id }).delete()
+
+    return response.json()
+  }
+
+  async index(request, response) {
+    const { user_id } = request.query
+
+    const movieNotes = await knex('movieNotes')
+      .where({ user_id })
+      .orderBy('title')
+
+    return response.json(movieNotes)
   }
 }
 
--- projeto-09/src/routes/movienotes.routes.js
+++ projeto-09/src/routes/movienotes.routes.js
@@ -6,6 +6,9 @@ const movieNotesRouter = require('../controllers/MovieNotesController')
 
 const movieNotesController = new movieNotesRouter()
 
-movieNotesRoutes.post("/:user_id", movieNotesController.create) 
+movieNotesRoutes.post('/:user_id', movieNotesController.create)
+movieNotesRoutes.get('/:id', movieNotesController.show)
+movieNotesRoutes.delete('/:id', movieNotesController.delete)
+movieNotesRoutes.get('/', movieNotesController.index)
 
 module.exports = movieNotesRoutes
